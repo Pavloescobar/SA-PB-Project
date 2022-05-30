8eea782d9e38135ffc8ed486805541f3df603d46
Pav <pavan_1994@hotmail.co.uk>
2022-05-30 12:51:00 +0100

    Auth, JS, css update

--- script.js
+++ script.js
@@ -1,47 +1,57 @@
 // selected all the elements for reference
 
-const apiKeyConsumer = `ck_8852c711cef4ad17f4ad29b1d47cc244158c89c9`;
-// need to place consideration to hide secret key
-const apiKeySecret = `cs_6ff05576b7061595dcaddea31de7f4c57bf1012c`;
+// the form to get apikey and apiSecert from the user
+CONFIG_FORM.addEventListener("submit", async function (event) {
+  event.preventDefault();
+
+  const data = {
+    apiKey: API_KEY_INPUT.value,
+    apiSecret: API_SECRET_INPUT.value,
+  };
+
+  const res = await fetch(
+    "https://shellective-woo.com/wp-json/wc/v3/products",
+    {
+      headers: {
+        Authorization:
+          "Basic " + btoa(`${API_KEY_INPUT.value}:${API_SECRET_INPUT.value}`),
+      },
+    }
+  );
+  const returnedData = await res.json();
+
+  CONFIG_FORM.style.display = "none";
+
+  // saving returned prouducts in products varialbe
+  products = [...returnedData];
+});
+
+// added a general function to to do data request and catch data back
+async function apiCall(url, body) {
+  const options = {
+    method: "POST",
+    headers: {
+      Accept: "text/html",
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: body,
+  };
+  const res = await fetch(url, options);
+  const returnedData = await res.json();
+  return returnedData;
+}
+
+// commented  the keys and secrete as we are getting it from user
+
+// const apiKeyConsumer = `ck_8852c711cef4ad17f4ad29b1d47cc244158c89c9`;
+// const apiKeySecret = `cs_6ff05576b7061595dcaddea31de7f4c57bf1012c`;
 let products = [];
 document.addEventListener("DOMContentLoaded", async function (e) {
   //
   e.preventDefault();
 
   // getting products data from WooCommerece REST APi
-  const res = await fetch(
-    "https://shellective-woo.com/wp-json/wc/v3/products",
-    {
-      headers: {
-        Authorization: "Basic " + btoa(`${apiKeyConsumer}:${apiKeySecret}`),
-      },
-    }
-  );
-  const data = await res.json();
-
-  // saving returned prouducts in products varialbe
-  products = [...data];
 });
-
-const LOGIN_FORM_SECTION = document.querySelector(".login-section");
-const LOGIN_FORM = document.querySelector("#login-form");
-const SPIN_SECTION = document.querySelector(".spin-section");
-const BTN_SPIN = document.querySelector("#spin");
-const VIEW_PORT = document.querySelector(".view-port");
-const EMAIL_INPUT = document.querySelector("#email");
-const BTN_LOGIN = document.querySelector("#btn-login");
-const IMG = document.querySelector("#selectedImg");
-const PRODUCT_NAME = document.querySelector("#product-name");
-
-/// data for to be selected randomly when clicking on Spin button // using local storage // no longer required due to using WooAPI
-// const data = [
-//   { src: "kl-bag", value: 20 },
-//   { src: "kl-black-hoodie", value: 34 },
-//   { src: "kl-black-joggers", value: 32 },
-//   { src: "kl-hat", value: 29 },
-//   { src: "kl-pink-joggers", value: 90 },
-//   { src: "kl-white-tee", value: 57 },
-// ];
 
 // function to get random number from min to max in our case (0,5)
 function getRandomInt(min, max) {
@@ -53,7 +63,7 @@ function getRandomInt(min, max) {
 /// event listener function when click on Spin button
 BTN_SPIN.addEventListener("click", async function () {
   // making api request and changing feedback accorindly
-  document.querySelector("#feedback").innerHTML = "Try your luck";
+  FEEDBACK_CONTAINER.innerHTML = "Try your luck";
   IMG.classList.add("loading");
 
   // checking if a user is logged in or not
@@ -66,56 +76,50 @@ BTN_SPIN.addEventListener("click", async function () {
     SPIN_SECTION.classList.add("hide");
   } else {
     // if logged in
-    /// considerdations being made for dynamic array length instead of defining static length, so we take method .length -1
     const random = getRandomInt(0, products.length - 1); //3
 
-    // selecting images and item randomly from returned prducts that we have
+    // selecting images and item randomily from returned prducts that we have
     const selectedImage = products[random].images[0].src;
 
-    IMG.setAttribute("src", selectedImage);
+    //
+
+    IMG.setAttribute("src", "./images/loading.gif");
+    PRODUCT_NAME.innerHTML = "loading...";
     const selectedItem = products[random];
 
     // composing request object and config for sending request to api
-    const options = {
-      method: "POST",
-      headers: {
-        Accept: "text/html",
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-      body: new URLSearchParams({
-        data: `{
-          "token": "RRftKA", 
-          "event": "Won Prize",
-          "customer_properties": {
-              "$email": "${localStorage.getItem("auth")}"
-          }, 
-          "properties": {
-              "item_name": "${selectedItem.name}",
-              "prize_image": "${selectedImage}",
-              "$value": ${Number(selectedItem.price)}
-          }
-      }`,
-      }),
-    };
 
-    // making actualy api call with async/await paradigme
+    const body = new URLSearchParams({
+      data: `{
+				"token": "RRftKA", 
+				"event": "Won Prize",
+				"customer_properties": {
+					"$email": "${localStorage.getItem("auth")}"
+				}, 
+				"properties": {
+					"item_name": "${selectedItem.name}",
+					"prize_image": "${selectedImage}",
+					"$value": ${Number(selectedItem.price)}
+				}
+			}`,
+    });
 
-    const res = await fetch("https://a.klaviyo.com/api/track", options);
-
-    const result = await res.json();
+    // making actual api call with async/await paradigme
+    const result = await apiCall("https://a.klaviyo.com/api/track", body);
 
     IMG.classList.remove("loading");
     PRODUCT_NAME.innerHTML = selectedItem.name;
+    IMG.setAttribute("src", selectedImage);
 
-    //// Writing success message displaying in the DOM, upon successful posting of selected item
+    //// writing congrats message upon successful posting of selected item
     console.log(result);
     if (result === 1) {
-      document.querySelector("#feedback").innerHTML = "Congratulations";
+      FEEDBACK_CONTAINER.innerHTML = "Congratulations";
     }
   }
 });
 
-///// even listener function of login functionality
+///// event listener function of login functionality
 LOGIN_FORM.addEventListener("submit", async function (e) {
   e.preventDefault();
 
@@ -123,26 +127,17 @@ LOGIN_FORM.addEventListener("submit", async function (e) {
   BTN_LOGIN.innerHTML = "sending...";
 
   /// request sending config again
-  const options = {
-    method: "POST",
-    headers: {
-      Accept: "text/html",
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-    body: new URLSearchParams({
-      data: `{"token": "RRftKA","properties": {"$email":"${EMAIL_INPUT.value}"}}`,
-    }),
-  };
+
+  const body = new URLSearchParams({
+    data: `{"token": "RRftKA","properties": {"$email":"${EMAIL_INPUT.value}"}}`,
+  });
 
   // making api call to indentify the user
-
-  const res = await fetch("https://a.klaviyo.com/api/identify", options);
-
-  const result = await res.json();
+  const result = await apiCall("https://a.klaviyo.com/api/identify", body);
 
   BTN_LOGIN.innerHTML = "Submit";
 
-  // if success, saving email in local storage, hiding login form section and showing spin section
+  // if success saving email in local storage, hiding login form section and showing spin section
   if (result === 1) {
     localStorage.setItem("auth", EMAIL_INPUT.value);
     LOGIN_FORM_SECTION.classList.add("hide");
